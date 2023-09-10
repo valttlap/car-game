bf9e0e320968ed69259c967a901c47cfc6f2b1ef
Valtteri Lappalainen <valtteri.lappalainen@gmail.com>
2023-09-10 18:04:36 +0300

    Added applicationextension

---
+++ Backend/CarGame.Api/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,41 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Reflection;
+using CarGame.Api.Exceptions;
+using Microsoft.OpenApi.Models;
+
+namespace CarGame.Api.Extensions;
+
+public static class ApplicationServiceExtensions
+{
+    public static IServiceCollection AddApplicationsServices(this IServiceCollection services, IConfiguration confing)
+    {
+        services.AddCors(options =>
+        {
+            options.AddDefaultPolicy(policy =>
+            {
+                policy.WithOrigins(confing["ClientUrl"] ?? throw new ConfigurationException("ClientUrl is not configured"))
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowCredentials()
+                    .SetPreflightMaxAge(TimeSpan.FromSeconds(86400));
+            });
+        });
+        services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+        services.AddSwaggerGen(opt =>
+        {
+            opt.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Version = "v1",
+                Title = "CarGame API",
+                Description = "CarGame API for CarGame application",
+            });
+
+            var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            opt.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFile));
+        });
+        services.AddSwaggerDocument();
+        return services;
+    }
+}
