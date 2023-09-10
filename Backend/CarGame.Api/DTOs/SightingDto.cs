367ad1592190a3985dfb63630b9d8a4bc8d42065
Valtteri Lappalainen <valtteri.lappalainen@gmail.com>
2023-09-10 21:33:16 +0300

    Add geojson serialization

--- Backend/CarGame.Api/DTOs/SightingDto.cs
+++ Backend/CarGame.Api/DTOs/SightingDto.cs
@@ -1,8 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using NetTopologySuite.Geometries;
-
 namespace CarGame.Api.DTOs;
 
 public class SightingDto
@@ -11,5 +9,5 @@ public class SightingDto
     public string? Description { get; set; }
     public bool IsDiplomat { get; set; }
     public int DiplomatNumber { get; set; }
-    public Point Location { get; set; } = default!;
+    public string Location { get; set; } = default!;
 }
--- Backend/CarGame.Api/Helpers/AutoMapperProfiles.cs
+++ Backend/CarGame.Api/Helpers/AutoMapperProfiles.cs
@@ -2,6 +2,10 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using AutoMapper;
+using CarGame.Api.DTOs;
+using CarGame.Api.Entites;
+using NetTopologySuite.Geometries;
+using NetTopologySuite.IO;
 
 namespace CarGame.Api.Helpers;
 
@@ -9,5 +13,19 @@ public class AutoMapperProfiles : Profile
 {
     public AutoMapperProfiles()
     {
+        CreateMap<Sighting, SightingDto>()
+            .ForMember(dest => dest.Location, opts => opts.MapFrom(src => GeometryToGeoJson(src.Location)))
+            .ReverseMap()
+            .ForMember(dest => dest.Location, opts => opts.MapFrom(src => GeoJsonToGeometry(src.Location)));
+    }
+
+    private static string? GeometryToGeoJson(Geometry geometry)
+    {
+        return geometry != null ? new GeoJsonWriter().Write(geometry) : null;
+    }
+
+    private static Geometry? GeoJsonToGeometry(string geoJson)
+    {
+        return !string.IsNullOrEmpty(geoJson) ? new GeoJsonReader().Read<Geometry>(geoJson) : null;
     }
 }
